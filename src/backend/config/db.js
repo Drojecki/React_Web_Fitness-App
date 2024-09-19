4cf68c9032f4f6d5cab7f70ef58b0abab784a92d
Drojecki <127322846+Drojecki@users.noreply.github.com>
2024-09-19 11:26:37 +0200

    db

---
+++ src/backend/config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err);
+    return;
+  }
+  console.log('Połączono z bazą danych MySQL');
+});
+
+module.exports = db;
